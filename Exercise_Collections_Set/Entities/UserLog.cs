ba3aa87d927017010269bd5a446b9ed189bac481
Thiago González <thiagogonzalez2000@gmail.com>
2022-06-03 11:01:22 -0300

    Add Set exercise to github

---
+++ Exercise_Collections_Set/Entities/UserLog.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Exercise_Collections_Set.Entities
+{
+    internal class UserLog
+    {
+        public string Username { get; private set; }
+        public DateTime AccessDate { get; private set; }
+
+        public UserLog(string username, DateTime accessDate)
+        {
+            Username = username;
+            AccessDate = accessDate;
+        }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is UserLog))
+            {
+                return false;
+            }
+
+            UserLog other = obj as UserLog;
+
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ Exercise_Collections_Set/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercise_Collections_Set.Entities;
+using System.Globalization;
+
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+HashSet<UserLog> set = new HashSet<UserLog>();
+
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] logData = sr.ReadLine().Split(' ');
+            string username = logData[0];
+            DateTime accessDate = DateTime.ParseExact(logData[1], "yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture);
+            set.Add(new UserLog(username, accessDate));
+        }
+        Console.WriteLine("Total users: " + set.Count);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
